25e083724895928b1f1331eaf128d062bbe8f431
Ali <ali@carriot.ir>
2022-05-22 22:41:56 +0430

    set reactions

--- src/com/company/reaction/Reaction.java
+++ src/com/company/reaction/Reaction.java
@@ -3,7 +3,6 @@ package com.company.reaction;
 import com.company.Player;
 
 public interface Reaction {
-    void execute();
     boolean isValidate();
     void punish(Player player);
     boolean isChallengable();
